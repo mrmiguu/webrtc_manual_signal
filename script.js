b2c988735c922ad16f8f1dc9e0dcc3ecb36971c2
Alex <a.matulionis@usa.com>
2020-11-30 17:37:59 -0800

    Abstract out RTC (separation of concerns)

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-import 'babel-polyfill'
+import "babel-polyfill"
 
 // Safari fix begin
 // Safari fix begin
 // Safari fix begin
 const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent)
-const fixSafariBtn = document.getElementById('fix-safari')
+const fixSafariBtn = document.getElementById("fix-safari")
 fixSafariBtn.hidden = !isSafari
 async function fixSafari() {
   await navigator.mediaDevices.getUserMedia({ audio: true })
@@ -17,52 +17,124 @@ window.fixSafari = fixSafari
 
 // ----------------------------------------------------------------
 
+/**
+ * @typedef Props
+ * @property {(ev: Event) => any} onOpen
+ * @property {(ev: MessageEvent<any>) => any} onMessage
+ * @property {(ev: Event) => any} onICEConnectionStateChange
+ */
+
+/**
+ * @param {Props}
+ */
+const newrtc = ({ onOpen, onMessage, onICEConnectionStateChange }) => {
+  const peer = new RTCPeerConnection({ iceServers: [{ urls: "stun:stun.1.google.com:19302" }] })
+  const chan = peer.createDataChannel("chat", { negotiated: true, id: 0 })
+  chan.onopen = onOpen
+  chan.onmessage = onMessage
+  peer.oniceconnectionstatechange = onICEConnectionStateChange
+  return { peer, chan }
+}
+
+/**
+ * @param {RTCPeerConnection} peer
+ */
+const newoffer = async peer => {
+  await peer.setLocalDescription(await peer.createOffer())
+  /** @type {string} */
+  const offer = await new Promise(resolve => {
+    peer.onicecandidate = ({ candidate }) => {
+      if (candidate) {
+        return
+      }
+      resolve(peer.localDescription.sdp)
+    }
+  })
+  return offer
+}
+
+/**
+ *
+ * @param {RTCPeerConnection} peer
+ * @param {string} offer
+ */
+const newanswer = async (peer, offer) => {
+  if (peer.signalingState !== "stable") {
+    throw new Error(`TODO: handle unstable signaling states (signalingState=${peer.signalingState})`)
+  }
+  await peer.setRemoteDescription({ type: "offer", sdp: offer })
+  await peer.setLocalDescription(await peer.createAnswer())
+  /** @type {string} */
+  const answer = await new Promise(resolve => {
+    peer.onicecandidate = ({ candidate }) => {
+      if (candidate) {
+        return
+      }
+      resolve(peer.localDescription.sdp)
+    }
+  })
+  return answer
+}
+
+/**
+ * @param {RTCPeerConnection} peer
+ * @param {string} answer
+ */
+const connectrtc = async (peer, answer) => {
+  if (peer.signalingState !== "have-local-offer") {
+    throw new Error(`TODO: handle signaling states without local offer (signalingState=${peer.signalingState})`)
+  }
+  await peer.setRemoteDescription({ type: "answer", sdp: answer })
+}
+
 // Manual signaling code starts here
 // Manual signaling code starts here
 // Manual signaling code starts here
 
-const config = { iceServers: [{ urls: 'stun:stun.1.google.com:19302' }] }
-const pc = new RTCPeerConnection(config)
-const dc = pc.createDataChannel('chat', { negotiated: true, id: 0 })
-const log = msg => div.innerHTML += `<br>${msg}`
-dc.onopen = () => chat.select()
-dc.onmessage = e => log(`> ${e.data}`)
-pc.oniceconnectionstatechange = e => log(pc.iceConnectionState)
+const log = msg => (div.innerHTML += `<br>${msg}`)
+
+const { peer: pc, chan: dc } = newrtc({
+  onOpen() {
+    console.log(`newrtc: onOpen`)
+    chat.select()
+  },
+  onMessage(e) {
+    console.log(`newrtc: onMessage`)
+    log(`> ${e.data}`)
+  },
+  onICEConnectionStateChange(e) {
+    console.log(`newrtc: onICEConnectionStateChange`)
+    log(pc.iceConnectionState)
+  },
+})
+
+async function createOffer() {
+  button.disabled = true
+  const o = await newoffer(pc)
+  offer.value = o
+  offer.select()
+  answer.placeholder = "Paste answer here"
+}
+window.createOffer = createOffer
+
+offer.onkeypress = async function (e) {
+  if (e.keyCode != 13) return
+  button.disabled = offer.disabled = true
+  const a = await newanswer(pc, offer.value)
+  answer.focus()
+  answer.value = a
+  answer.select()
+}
+
+answer.onkeypress = async function (e) {
+  if (e.keyCode != 13) return
+  answer.disabled = true
+  await connectrtc(pc, answer.value)
+}
 
 chat.onkeypress = function (e) {
   if (e.keyCode != 13) return
   dc.send(chat.value)
   log(chat.value)
-  chat.value = ''
-}
-
-async function createOffer() {
-  button.disabled = true
-  await pc.setLocalDescription(await pc.createOffer())
-  pc.onicecandidate = ({ candidate }) => {
-    if (candidate) return
-    offer.value = pc.localDescription.sdp
-    offer.select()
-    answer.placeholder = 'Paste answer here'
-  }
-}
-window.createOffer = createOffer
-
-offer.onkeypress = async function (e) {
-  if (e.keyCode != 13 || pc.signalingState != 'stable') return
-  button.disabled = offer.disabled = true
-  await pc.setRemoteDescription({ type: 'offer', sdp: offer.value })
-  await pc.setLocalDescription(await pc.createAnswer())
-  pc.onicecandidate = ({ candidate }) => {
-    if (candidate) return
-    answer.focus()
-    answer.value = pc.localDescription.sdp
-    answer.select()
-  }
-}
-
-answer.onkeypress = function (e) {
-  if (e.keyCode != 13 || pc.signalingState != 'have-local-offer') return
-  answer.disabled = true
-  pc.setRemoteDescription({ type: 'answer', sdp: answer.value })
+  chat.value = ""
 }
