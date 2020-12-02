ddca5e119600db1674caf256779741d37a4e7fbc
Alex <a.matulionis@usa.com>
2020-12-01 22:20:44 -0800

    Clean up comments

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,8 @@ import { offerer, answerer } from "./rtc"
 
 const { log } = console
 
-// Safari fix begin
-// Safari fix begin
-// Safari fix begin
-
 const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent)
-
 isSafari && navigator.mediaDevices.getUserMedia({ audio: true })
-
-// Safari fix end
-// Safari fix end
-// Safari fix end
-
-// ----------------------------------------------------------------
-
-// Manual signaling code starts here
-// Manual signaling code starts here
-// Manual signaling code starts here
 
 declare global {
   interface Window {
